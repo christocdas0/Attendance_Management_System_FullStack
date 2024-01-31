dfe88ef4dc9ee511c282c359a0b91aa67c661a28
christoc <christocdas0@gmail.com>
2024-01-31 18:54:25 +0530

    Removed Unwanted Comments

--- ui/loginPage.js
+++ ui/loginPage.js
@@ -31,8 +31,6 @@ function validateLogin() {
 }
 
 function validateUser() {
-  // var username = document.getElementById("username").value;
-  // var password = document.getElementById("password").value;
   loginBtn.style.display = "none";
   validateTxt.style.display = "flex";
   getStudentData();
